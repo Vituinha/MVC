e2dba392662179fdb7a2c39fd3e0a351f18c4f26
Vituinha <56604707+Vituinha@users.noreply.github.com>
2023-05-02 23:06:28 -0300

    Alterando MVC
    Adicionando conteúdo para as Views e Criando um Controller para o MVC em construção.

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -2,15 +2,24 @@
 namespace App\Controller\Pages;
 
 use \App\Utils\View;
+use \App\Model\Entity\Organization;
 
-class Home {
+class Home extends Page{
     /** 
     *Método responsável por retornar o conteúdo (view) da nossa home
     *@return string
     */
     public static function getHome(){
-        return View::render('Pages/home',[
-        "name" => "WDEV - Canal",
-        "description" => "Canal do youtube: https://youtube.com.br/wdevoficial"]);
+        //ORGANIZAÇÃO
+        $obOrganization = new Organization;
+
+        //RETORNA A VIEW DA HOME
+        $content = View::render('Pages/Home',[
+        "name" => $obOrganization->name,
+        "description" => $obOrganization->description,
+        "site" => $obOrganization->site]);
+
+        //RETORNA A VIEW DA PÁGINA
+        return parent::getPage('WDEV - CANAL - HOME', $content);
     }
 }
